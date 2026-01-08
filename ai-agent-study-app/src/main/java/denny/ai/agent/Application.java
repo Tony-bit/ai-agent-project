cff9927d3028b8e30dadba2a1cca5111fa3ab719
Denny <583915955@qq.com>
2026-01-08 10:54:48 +0800

    ai-agent-study

--- ai-agent-study-app/src/main/java/denny/ai/agent/Application.java
+++ ai-agent-study-app/src/main/java/denny/ai/agent/Application.java
@@ -1,12 +1,8 @@
 package denny.ai.agent;
 
-import org.springframework.ai.model.openai.autoconfigure.OpenAiAudioSpeechAutoConfiguration;
-import org.springframework.ai.model.openai.autoconfigure.OpenAiAudioTranscriptionAutoConfiguration;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = {OpenAiAudioSpeechAutoConfiguration.class, OpenAiAudioTranscriptionAutoConfiguration.class})
 @Configurable
 public class Application {
 
