72515269cfb352e3f6cd927e136efe0adfc1b74c
Denny <583915955@qq.com>
2026-03-10 14:34:00 +0800

    ai-agent-study: 支持混合检索+RRF评分+rerank

--- ai-agent-study-app/src/main/java/denny/ai/agent/Application.java
+++ ai-agent-study-app/src/main/java/denny/ai/agent/Application.java
@@ -1,13 +1,15 @@
 package denny.ai.agent;
 
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@Configurable
+@SpringBootApplication
+@EnableScheduling
 public class Application {
 
     public static void main(String[] args){
-        SpringApplication.run(Application.class);
+        SpringApplication.run(Application.class, args);
     }
 
 }
